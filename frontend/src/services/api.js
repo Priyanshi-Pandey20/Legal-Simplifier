d25a5cd8fcddbd586f490c135fed8274ba0d3632
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-06-23 12:39:32 +0530

    fix hardcoded localhost url and add frontend env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -25,6 +25,6 @@ export const getHistory   = ()     => API.get('/api/history');
 export const changePassword = (data) => API.post('/api/auth/change-password', data);
 export const deleteHistory = (id) => API.delete(`/api/document/${id}`)
 export const uploadFile = (formData) => API.post('/api/document/upload-file',formData);
-export const getDocumentFile = (filename) =>  `http://localhost:5000/api/document/file/${filename}`;
+export const getDocumentFile = (filename) => `${process.env.REACT_APP_API_URL || 'http://localhost:5000'}/api/document/file/${filename}`;
 
 export default API;
